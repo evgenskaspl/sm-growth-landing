9eff7990790edeb81c23fb60751241f77040d803
Yauheni Skaskevich <evgenskaspl@gmail.com>
2026-06-01 22:04:47 +0200

    fix logo

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'serve-index-html-route',
+      configureServer(server) {
+        server.middlewares.use('/index.html', (_req, res, next) => {
+          // rewrite to SPA root so React Router handles it
+          _req.url = '/'
+          next()
+        })
+      },
+    },
+  ],
 })
